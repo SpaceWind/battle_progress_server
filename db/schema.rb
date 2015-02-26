07e02896a37827f9471028247c969c879f3fd13e
nother <exehot@gmail.com>
2015-02-26 17:27:10 +0300

    lib controller setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226084408) do
+ActiveRecord::Schema.define(version: 20150226134123) do
 
   create_table "active_keys", force: true do |t|
     t.string   "apikey"
@@ -73,6 +73,14 @@ ActiveRecord::Schema.define(version: 20150226084408) do
     t.integer  "y_pos"
     t.string   "hclass"
     t.string   "hrace"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "libs", force: true do |t|
+    t.string   "token"
+    t.string   "item"
+    t.text     "value"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
